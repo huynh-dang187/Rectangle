30f065bf1ef58011014eff39c4bb1fba796df5e3
Huynh Dang <nguyendangchanell@gmail.com>
2024-03-10 22:49:25 +0700

    CODE BAI 1 chuong 3

---
+++ Rectangle/Rectangle/Rectangle.cpp
@@ -0,0 +1,39 @@
+﻿// Rectangle.cpp : Defines the entry point for the application.
+//
+
+#include "Rectangle.h"
+
+using namespace std;
+int CRectangle::getHeight() const {
+	return height;
+}
+int CRectangle::getWidth() const {
+	return width; 
+}	
+void CRectangle::setHeight(int _height) {
+	height = _height;
+}
+void CRectangle::setWidth(int _width) {
+	width = _width;
+}
+int CRectangle::area() {
+	return width*height;
+}
+int main()
+{
+	CRectangle rect1, * rect2;
+	//rect1.height = 2 ;
+	rect1.setHeight(3);
+	rect1.setWidth(4);
+	cout << "Height1 : " << rect1.getHeight() << endl;
+	cout << "Width1 : " << rect1.getWidth() << endl;
+	cout << "S1: " << rect1.area() << endl;
+	cout << endl;
+	rect2 = new CRectangle();
+	rect2->setHeight(5);
+	rect2->setWidth(4);
+	cout << "Height2 : " << rect2->getHeight() << endl;
+	cout << "Width2 : " << rect2->getWidth() << endl;
+	cout << "S2: " << rect2->area() << endl;
+	return 0;
+}
---
+++ Rectangle/Rectangle/Rectangle.h
@@ -0,0 +1,19 @@
+﻿// Rectangle.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+class CRectangle {
+private : 
+	int width, height; 
+public : 
+	void setWidth(int _width);
+	int getWidth() const;
+	void setHeight(int _height);
+	int getHeight() const; 
+	int area();
+};
+
+// TODO: Reference additional headers your program requires here.
